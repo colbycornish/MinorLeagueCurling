b478afd9acaca8d370e714f5d0c5a7263b2f95a1
colbycornish <taylor@culbertsons.com>
2025-02-24 17:42:18 -0800

    Add folders for various Menus

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -5,9 +5,37 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-
+    /// <summary>
+    /// These are the scripts used for the Main Menu Screen that starts the game
+    /// </summary>
 
     public void LoadGame(){
-        SceneManager.LoadScene(1);
+        // Opens up the 'start new game' experience
+        SceneManager.LoadScene(sceneName: "Scene_Curling");
+    }
+
+
+    public void LaunchContinueGame(){
+        // Opens up the 'ContinueGame' experience
+        // TODO: Create launcher script for a continue game experience
+        SceneManager.LoadScene(sceneName: "Scene_Curling");
+    }
+
+    public void LaunchSettings(){
+        // Opens up the settings screen
+        // TODO: Create launcher script for a settings experience
+        // TODO: Create a global settings file
+
+        SceneManager.LoadScene(sceneName: "Scene_Curling");
+    }
+
+    public void LaunchMultiplayer(){
+        // Opens up the multiplayer menu
+        SceneManager.LoadScene(sceneName: "Scene_Curling");
+    }
+
+    public void LaunchNewGame(){
+        // Opens up the 'start new game' experience
+        SceneManager.LoadScene(sceneName: "Scene_Curling");
     }
 }
---
+++ Assets/Scripts/Menu/MenuSettings.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuSettings : MonoBehaviour
+{
+
+
+    public void LoadSettings(){
+        // Loads the existing User settings file, that determines things like:
+        // - Audio levels
+        // - Displays
+        //  - Brightness
+        //  - Gamma
+        // - Controls
+        //  - Controller
+        // - Platform
+        // - Difficulty
+        // - Dev Settings & Features
+        
+        
+    }
+
+}
