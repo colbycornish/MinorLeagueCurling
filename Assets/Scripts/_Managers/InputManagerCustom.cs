ca168e4df8b2af6d0bd7e63873a9e5b14c82b683
colbycornish <taylor@culbertsons.com>
2025-07-18 10:17:16 -0700

    Add feel to plugins

---
+++ Assets/Scripts/_Managers/InputManagerCustom.cs
@@ -0,0 +1,41 @@
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System;
+
+/// <summary>
+/// Singleton GameManager that handles scene transitions, player spawn positioning, and persistent data.
+/// </summary>
+
+
+
+
+public class InputManagerCustom : MonoBehaviour
+{
+    
+    // public KeyCode resetKey = KeyCode.R;
+    // public KeyCode rightCurlKey = KeyCode.E;
+    // public KeyCode leftCurlKey = KeyCode.Q;
+    // public KeyCode rightSweeperKey = KeyCode.L; // Action button for right sweeper sweeping ** NEW SWEEPER CODE **
+    // public KeyCode leftSweeperKey = KeyCode.K; // Action button for left sweeper sweeping ** NEW SWEEPER CODE **
+    // public KeyCode actionKey = KeyCode.Space; 
+
+
+    // void Awake()
+    // {
+    //     if (_instance == null)
+    //     {
+    //         DontDestroyOnLoad(gameObject);
+    //         _instance = this;
+    //     }
+    //     else
+    //     {
+    //         Destroy(gameObject);
+    //     }
+    //     isReady = true;
+    // }
+    
+    
+
+}
